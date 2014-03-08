3b17da8bed4b9a052478d7c854effc8de8ae3880
Ondra Gersl <ondra.gersl@gmail.com>
2014-03-08 14:17:49 +0100

    Add install.sh file

--- install.sh
+++ install.sh
@@ -1,1 +1,46 @@
 #!/bin/sh
+{
+	CONFIG_ARCHIVE_URL='http://config.ondra.biz/config.tgz'
+	CONFIG_ARCHIVE_FILE='config.tgz'
+
+	INI_FILE='config.ini'
+	TMP_DIR=$(mktemp -d)
+
+	# download and extract files
+	wget -q -O "$TMP_DIR/$CONFIG_ARCHIVE_FILE" "$CONFIG_ARCHIVE_URL"
+	if [ $? -ne 0 ]; then
+		echo "ERROR! Can't download config files. Check URL in '$0' file." >&2
+		exit 1
+	fi
+	tar xzf "$TMP_DIR/$CONFIG_ARCHIVE_FILE" -C "$TMP_DIR"
+
+	# parse ini file
+	# source: http://mark.aufflick.com/blog/2007/11/08/parsing-ini-files-with-sed
+	PARSED_INI=`sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+		-e 's/;.*$//' \
+		-e 's/[[:space:]]*$//' \
+		-e 's/^[[:space:]]*//' \
+		-e "s/^\(.*\)=\([^\"']*\)$/\1=\2/" \
+		< $TMP_DIR/$INI_FILE`
+
+	# install scripts
+	for section in $(echo "$PARSED_INI" | grep '^\[' | sed -e 's/^\[\(.*\)\]/\1/'); do
+		echo "Installing '$section' configs..."
+		for config in $(echo "$PARSED_INI" | sed -n -e "/^\[$section\]/,/^\s*\[/{/^[^;].*\=.*/p;}"); do
+			src_file=`echo "$config" | cut -d'=' -f1`
+			eval dst_file=`echo "$config" | cut -d'=' -f2`
+			if [ -f "$dst_file" ]; then
+				backup_file=$(date "+$dst_file.%Y-%m-%d_%H-%M-%S")
+				echo "  moving old '$dst_file' file to '$backup_file'"
+				mv "$dst_file" "$backup_file"
+			fi
+			echo "  copy '$TMP_DIR/$section/$src_file' into '$dst_file'"
+			cp "$TMP_DIR/$section/$src_file" "$dst_file"
+		done
+	done
+
+	# cleanup
+	rm -rf "$TMP_DIR"
+
+	echo "Done."
+}
